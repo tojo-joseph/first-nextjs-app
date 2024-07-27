b924b5db18e9efc5a7e595647ade9a4b3ac409e7
tojo-delnie <116822789+tojo-delnie@users.noreply.github.com>
2024-07-27 18:16:25 +0530

    commit check

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>🔥 Let&apos;s get started 🔥</p>
       <p>
         <Link href="/about">About Us</Link>
       </p>
